529e1b1cba744e8fe0241d69e2a4f9fa3aa71d99
ErezHackeru <45459156+ErezHackeru@users.noreply.github.com>
2019-08-04 15:14:09 +0300

    Add files via upload

---
+++ HW3107_ContentPresenter_FlightSystem/HW3107_ContentPresenter_FlightSystem/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using FlightsSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW3107_ContentPresenter_FlightSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Flight flight { get; set; }
+
+        static ILoginToken IloginAnonymous = FlyingCenterSystem.GetFlyingCenterSystemInstance().Login("NoMatch", "1111");
+        //actually we send null in this case which is why we get anonymousUseFacade in return.
+        AnonymousUserFacade anonymousUserFacade = (AnonymousUserFacade)FlyingCenterSystem.GetFlyingCenterSystemInstance().GetFacade(IloginAnonymous);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            flight = anonymousUserFacade.GetFlightByID(40092);
+            this.DataContext = this;
+        }
+
+    }
+}
