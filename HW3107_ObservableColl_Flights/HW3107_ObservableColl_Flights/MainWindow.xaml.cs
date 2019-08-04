47434c05cc2a435f188fa693538eabeaac35058a
ErezHackeru <45459156+ErezHackeru@users.noreply.github.com>
2019-08-04 13:28:29 +0300

    Add files via upload

---
+++ HW3107_ObservableColl_Flights/HW3107_ObservableColl_Flights/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using FlightsSystem;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW3107_ObservableColl_Flights
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<AirlineCompany> airLineCis = new ObservableCollection<AirlineCompany>();
+
+        static ILoginToken IloginAnonymous = FlyingCenterSystem.GetFlyingCenterSystemInstance().Login("NoMatch", "1111");
+        //actually we send null in this case which is why we get anonymousUseFacade in return.
+        AnonymousUserFacade anonymousUserFacade = (AnonymousUserFacade)FlyingCenterSystem.GetFlyingCenterSystemInstance().GetFacade(IloginAnonymous);
+
+        public MainWindow()
+        {
+            this.DataContext = airLineCis;
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            IList<AirlineCompany> ALC = anonymousUserFacade.GetAllAirLineCompanies();//flightNametxtBx.Text
+            foreach (AirlineCompany a in ALC)
+            {
+                if (a.AirLineName == airLineNametxtBx.Text)
+                {
+                    airLineCis.Add(a);
+                }
+            }
+        }
+    }
+}
